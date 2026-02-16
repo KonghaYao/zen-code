b82fbac34ab66ee28c31d347bb04a6888e018b9e
KonghaYao <3446798488@qq.com>
2026-02-16 21:52:15 +0800

    fix: 删除打印

--- packages/agent/src/middlewares/subTasks.ts
+++ packages/agent/src/middlewares/subTasks.ts
@@ -62,7 +62,6 @@ export class SubAgentsMiddleware implements AgentMiddleware {
         this.agentList = this.formatSubAgentsList(pkg);
         this.tools.push(
             create_task_tool(async (taskId, args, state) => {
-                console.log(state.model_id);
                 return await createStandardAgentV2(args.subagent_id, pkg, state, {}, { parent_id: taskId });
             }),
         );
